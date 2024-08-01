b18e715821e166ebc2750799d5a796c9c5a864c8
Guillermo Monzó <gcmonzo@gmv.com>
2024-08-01 14:49:35 +0200

    fix: remove from repo generated files

--- projects/mapper-api-client/model/models.ts
+++
@@ -1,6 +0,0 @@
-export * from './link';
-export * from './ontologyDTO';
-export * from './pageSearchOntologyDTO';
-export * from './pageableObject';
-export * from './searchOntologyDTO';
-export * from './sortObject';
