1caaa7bb10ce1782bb74cb6b99475f44c67d5211
SSRV <sandra.rodriguez.v@gmv.com>
2024-08-02 13:57:51 +0200

    Manahe edit/add modal

--- src/app/pages/ontologies/ontologies-list/ontologies-list.component.ts
+++ src/app/pages/ontologies/ontologies-list/ontologies-list.component.ts
@@ -2,7 +2,8 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { OntologyService, PageSearchOntologyDTO, SearchOntologyDTO } from 'projects/mapper-api-client';
 import { catchError, of } from 'rxjs';
-import { PAGE, SIZE } from 'src/app/shared/utils/app.constants';
+import { LanguageService } from 'src/app/shared/services/language.service';
+import { ONTOLOGIES_ADD_ONTOLOGY, ONTOLOGIES_EDIT_ONTOLOGY, PAGE, SIZE } from 'src/app/shared/utils/app.constants';
 
 @Component({
 	selector: 'app-ontologies-list',
@@ -12,9 +13,12 @@ export class OntologiesListComponent implements OnInit {
 
 	destroyRef = inject(DestroyRef);
 
-	constructor(private ontologyService: OntologyService) { }
+	constructor(private ontologyService: OntologyService, private languageService: LanguageService) { }
 
 	ontologies: SearchOntologyDTO[];
+	selectedOntology: SearchOntologyDTO | null = null;
+	header: string = '';
+	isEditMode: boolean = false;
 	visible: boolean = false;
 
 	/**
@@ -22,6 +26,16 @@ export class OntologiesListComponent implements OnInit {
 	 */
 	ngOnInit(): void {
 		this.loadOntologies();
+	}
+
+	/**
+	 * Shows the dialog for adding/editing an ontology
+	 */
+	showDialog(ontology?: SearchOntologyDTO): void {
+		this.selectedOntology = ontology ? { ...ontology } : null;
+		this.isEditMode = !!ontology;
+		this.header = ontology ? this.languageService.translateValue(ONTOLOGIES_EDIT_ONTOLOGY) : this.languageService.translateValue(ONTOLOGIES_ADD_ONTOLOGY);
+		this.visible = true;
 	}
 
 	/**
@@ -40,12 +54,5 @@ export class OntologiesListComponent implements OnInit {
 			.subscribe((data: PageSearchOntologyDTO) => {
 				this.ontologies = data.content ?? [];
 			});
-	}
-
-	/**
-	 * Shows the dialog for adding a new ontology
-	 */
-	showDialog() {
-		this.visible = true;
 	}
 }
