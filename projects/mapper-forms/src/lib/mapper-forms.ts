421cb93f58455a60544ea944259bb985cb7a3d47
SSRV <sandra.rodriguez.v@gmv.com>
2024-08-09 13:58:25 +0200

    List/delete data sources

--- projects/mapper-forms/src/lib/mapper-forms.ts
+++ projects/mapper-forms/src/lib/mapper-forms.ts
@@ -1,25 +1,81 @@
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+
+export const dataSourceDtoForm = new FormGroup({
+	connectionString: new FormControl(null, []),
+	databaseType: new FormControl(null, []),
+	fileName: new FormControl(null, []),
+	id: new FormControl(null, []),
+	name: new FormControl(null, []),
+	password: new FormControl(null, []),
+	type: new FormControl(null, []),
+	user: new FormControl(null, []),
+	version: new FormControl(null, [])
+});
 
 export const linkForm = new FormGroup({
-  href: new FormControl(null, []),
-  templated: new FormControl(null, [])
+	href: new FormControl(null, []),
+	templated: new FormControl(null, [])
 });
 
 export const ontologyDtoForm = new FormGroup({
-  content: new FormControl(null, []),
-  id: new FormControl(null, []),
-  name: new FormControl(null, []),
-  title: new FormControl(null, []),
-  uploadDate: new FormControl(null, []),
-  url: new FormControl(null, []),
-  version: new FormControl(null, []),
-  versionName: new FormControl(null, [])
+	content: new FormControl(null, []),
+	id: new FormControl(null, []),
+	name: new FormControl(null, []),
+	title: new FormControl(null, []),
+	uploadDate: new FormControl(null, []),
+	url: new FormControl(null, []),
+	version: new FormControl(null, []),
+	versionName: new FormControl(null, [])
+});
+
+export const pageDataSourceDtoForm = new FormGroup({
+	content: new FormControl(null, []),
+	empty: new FormControl(null, []),
+	first: new FormControl(null, []),
+	last: new FormControl(null, []),
+	number: new FormControl(null, []),
+	numberOfElements: new FormControl(null, []),
+	pageable: new FormControl(null, []),
+	size: new FormControl(null, []),
+	sort: new FormControl(null, []),
+	totalElements: new FormControl(null, []),
+	totalPages: new FormControl(null, [])
+});
+
+export const pageSearchOntologyDtoForm = new FormGroup({
+	content: new FormControl(null, []),
+	empty: new FormControl(null, []),
+	first: new FormControl(null, []),
+	last: new FormControl(null, []),
+	number: new FormControl(null, []),
+	numberOfElements: new FormControl(null, []),
+	pageable: new FormControl(null, []),
+	size: new FormControl(null, []),
+	sort: new FormControl(null, []),
+	totalElements: new FormControl(null, []),
+	totalPages: new FormControl(null, [])
+});
+
+export const pageableObjectForm = new FormGroup({
+	offset: new FormControl(null, []),
+	pageNumber: new FormControl(null, []),
+	pageSize: new FormControl(null, []),
+	paged: new FormControl(null, []),
+	sort: new FormControl(null, []),
+	unpaged: new FormControl(null, [])
 });
 
 export const searchOntologyDtoForm = new FormGroup({
-  name: new FormControl(null, []),
-  title: new FormControl(null, []),
-  uploadDate: new FormControl(null, []),
-  url: new FormControl(null, []),
-  versionName: new FormControl(null, [])
+	id: new FormControl(null, []),
+	name: new FormControl(null, []),
+	title: new FormControl(null, []),
+	uploadDate: new FormControl(null, []),
+	url: new FormControl(null, []),
+	versionName: new FormControl(null, [])
+});
+
+export const sortObjectForm = new FormGroup({
+	empty: new FormControl(null, []),
+	sorted: new FormControl(null, []),
+	unsorted: new FormControl(null, [])
 });
--- src/app/pages/data-sources/data-sources-list/data-sources-list.components.ts
+++ src/app/pages/data-sources/data-sources-list/data-sources-list.components.ts
@@ -1,26 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { DataSourceDTO, DataSourceService, PageDataSourceDTO } from 'projects/mapper-api-client';
+import { LanguageService } from 'src/app/shared/services/language.service';
+import { DATA_SOURCES_ADD_DATA_SOURCE, DATA_SOURCES_EDIT_DATA_SOURCE, PAGE, SIZE } from 'src/app/shared/utils/app.constants';
 
 @Component({
 	selector: 'app-data-sources-list',
 	templateUrl: './data-sources-list.component.html'
 })
-export class DataSourcesListComponent {
-	datasources = [
-		{ fuente: 'Fuente de datos', formato: 'CSV' },
-		{ fuente: 'Fuente de datos', formato: 'CSV' },
-		{ fuente: 'Fuente de datos', formato: 'CSV' },
-		{ fuente: 'Fuente de datos', formato: 'CSV' },
-		{ fuente: 'Fuente de datos', formato: 'CSV' },
-		{ fuente: 'Fuente de datos', formato: 'CSV' }
-	];
+export class DataSourcesListComponent implements OnInit {
+
+	destroyRef = inject(DestroyRef);
+
+	constructor(private dataSourceService: DataSourceService, private languageService: LanguageService) { }
+
+	dataSources: DataSourceDTO[];
+	selectedDataSource: DataSourceDTO;
+
+	header: string = '';
+	isEditMode: boolean = false;
+	visible: boolean = false;
 	addDialogVisible: boolean = false;
 	deleteDialogVisible: boolean = false;
 
-	showDialog() {
+	/**
+	 * Loads the data sources when the component is initialized
+	 */
+	ngOnInit(): void {
+		this.loadDataSources();
+	}
+
+	/**
+	 * Loads the data sources list.
+	 */
+	loadDataSources(): void {
+		this.dataSourceService
+			.listDataSources(PAGE, SIZE)
+			.pipe(
+				//TODO: pagination and show success/error popup
+				takeUntilDestroyed(this.destroyRef)
+			)
+			.subscribe((data: PageDataSourceDTO) => {
+				this.dataSources = data.content ?? [];
+			});
+	}
+
+	/**
+	 * Delete data source by its id.
+	 */
+	deleteDataSource(id: number): void {
+		this.dataSourceService
+			.deleteDataSource(id)
+			.pipe(
+				//TODO: action confirmation popup
+				takeUntilDestroyed(this.destroyRef)
+			)
+			.subscribe(() => {
+				this.dataSources = this.dataSources.filter((dataSource) => dataSource.id !== id);
+				console.log('Data source deleted');
+			});
+		this.deleteDialogVisible = false;
+	}
+
+	showDialog(dataSource?: DataSourceDTO): void {
+		this.header = dataSource
+			? this.languageService.translateValue(DATA_SOURCES_EDIT_DATA_SOURCE)
+			: this.languageService.translateValue(DATA_SOURCES_ADD_DATA_SOURCE);
 		this.addDialogVisible = true;
 	}
 
-	showDialogDelete() {
+	showDialogDelete(dataSource: DataSourceDTO): void {
+		this.selectedDataSource = dataSource;
 		this.deleteDialogVisible = true;
+	}
+
+	cancelDelete(): void {
+		this.deleteDialogVisible = false;
 	}
 }
