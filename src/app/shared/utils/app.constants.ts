d2dfcc47611de4194513e68338a4d676bc0dcb9e
SSRV <sandra.rodriguez.v@gmv.com>
2024-10-08 16:29:19 +0200

    Fix mapping name and add success message

--- src/app/shared/utils/app.constants.ts
+++ src/app/shared/utils/app.constants.ts
@@ -29,6 +29,7 @@ export const MESSAGES_DATA_SOURCES_SUCCESS_CREATED: string = translate('messages
 export const MESSAGES_DATA_SOURCES_SUCCESS_UPDATED: string = translate('messages.data-sources.success.updated');
 export const MESSAGES_DATA_SOURCES_SUCCESS_DELETED: string = translate('messages.data-sources.success.deleted');
 
+export const MESSAGES_MAPPINGS_SUCCESS_CREATED: string = translate('messages.mappings.success.created');
 export const MESSAGES_MAPPINGS_SUCCESS_DELETED: string = translate('messages.mappings.success.deleted');
 export const MESSAGES_MAPPINGS_SUCCESS_UPDATED: string = translate('messages.mappings.success.updated');
 export const MESSAGES_MAPPINGS_PAIRS: string = translate('messages.mappings.pairs');
